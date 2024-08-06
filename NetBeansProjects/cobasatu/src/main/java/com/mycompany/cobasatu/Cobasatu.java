253a4e4a2a8d31702d2638b6e01648cec97aae5f
okxyL <141798856+okxyL@users.noreply.github.com>
2024-08-06 15:27:14 +0700

    modul Pemograman Visual

---
+++ NetBeansProjects/cobasatu/src/main/java/com/mycompany/cobasatu/Cobasatu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cobasatu;
+
+/**
+ *
+ * @author hasbi
+ */
+public class Cobasatu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
